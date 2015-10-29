a2e8400093f36fd44a637cf339be2032dfd007bd
StefanoMagrassi <stefano.magrassi@gmail.com>
2015-10-29 15:29:36 +0100

    libs tests

--- tests/test.utils.js
+++ tests/test.utils.js
@@ -19,32 +19,3 @@ test('convert function "arguments" into array', function(t) {
   t.deepEqual(f('a', 0, {b: 'beta'}), ['a', 0, {b: 'beta'}]);
   t.end();
 });
-
-// utils.longestKey
-// ---
-test('get the longest key in object', function(t) {
-  var shortKey = 'foo',
-      longKey  = 'very_very_long',
-      o        = {};
-  
-  o[shortKey] = shortKey;
-  o[longKey]  = longKey;
-  
-  t.comment('short key');
-  t.notEqual(utils.longestKey(o), shortKey.length);
-  t.comment('long key');
-  t.equal(utils.longestKey(o), longKey.length);
-  t.end();
-});
-
-// utils.filler
-// ---
-test('add trailing pad to string until it reaches maximum length', function(t) {
-  var shortKey = 'foo',
-      longKey  = 'very_very_long',
-      maximum  = longKey.length;
-  
-  t.comment('short key must be long as long key');
-  t.equal(utils.filler(shortKey, maximum).length, longKey.length);
-  t.end();
-});
